27ea20395da04bf21722b369efb6e7b9ceca71fb
lucas costa <lucaszcostaste4@gmail.com>
2024-02-13 15:06:37 -0300

    primeiro commit, projeto base

---
+++ ProjetoBandasOO/Musica.cs
@@ -0,0 +1,41 @@
+﻿
+class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    private bool disponivel;
+
+
+    public void EscreveDisponivel(bool value)
+    {
+        disponivel = value;
+    }
+
+    public void LeituraDisponivel(bool value)
+    {
+        disponivel = value;
+    }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome da musica: {nome}");
+        Console.WriteLine($"Nome do artista: {artista}");
+        Console.WriteLine($"Duração da musica: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano atual");
+        }
+        else
+        {
+            Console.WriteLine("Não disponivel no plano atual.\nAdquira o plano Plus+");
+        }
+
+    }
+
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"{artista} - {nome}");
+    }
+
+}
--- ProjetoBandasOO/Program.cs
+++ ProjetoBandasOO/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+Musica musica1 = new Musica();
+musica1.nome = "In the End";
+musica1.artista = "Linkin park";
+musica1.duracao = 300;
+
+
+Musica musica2  = new Musica();
+musica2.nome = "Vermillion";
+musica2.artista = "Slipknot";
+musica1.duracao = 400;
+
+
+musica1.ExibirFichaTecnica();
