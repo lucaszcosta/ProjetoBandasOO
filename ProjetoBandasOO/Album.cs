0d2ce57e4a858a2ba7660491b500d8fd197772dd
lucas costa <lucaszcostaste4@gmail.com>
2024-02-13 16:31:33 -0300

    editando a duracao total do album

--- ProjetoBandasOO/Album.cs
+++ ProjetoBandasOO/Album.cs
@@ -4,10 +4,26 @@ class Album
 {
     private List<Musica> musicas = new List<Musica>();
     public string Nome { get; set; }
-    public int DuracaoTotal { get; set; }
+    public int DuracaoTotal
+    {
+        get => DuracaoTotal; set
+        {
+
+        }
+
+    }
 
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
+    }
+
+    public void ExibirMusicasAlbum()
+    {
+        Console.WriteLine($"Lista de musicas do album {Nome}:\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Musica: {musica.Nome}");
+        }
     }
 }
--- ProjetoBandasOO/Program.cs
+++ ProjetoBandasOO/Program.cs
@@ -1,19 +1,17 @@
-﻿
+﻿Album albumTeste = new Album();
+
+albumTeste.Nome = "Survival city";
+
 Musica musica1 = new Musica();
 musica1.Nome = "In the End";
-musica1.Artista = "Linkin park";
-musica1.Duracao = 300;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.Disponivel);
+musica1.Duracao = 123;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Disturbed";
+musica2.Duracao = 300;
 
 
-Musica musica2  = new Musica();
-musica2.Nome = "Vermillion";
-musica2.Artista = "Slipknot";
-musica1.Duracao = 400;
-musica1.Disponivel = false;
+albumTeste.AdicionarMusica(musica1);
+albumTeste.AdicionarMusica(musica2);
 
-
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-
+albumTeste.ExibirMusicasAlbum();
